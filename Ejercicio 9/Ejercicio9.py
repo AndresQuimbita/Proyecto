3b802e27dae6837dcaf83e0bcf449fc697fb9e93
AndresQuimbita <63878124+AndresQuimbita@users.noreply.github.com>
2021-12-06 19:50:27 -0500

    Update Ejercicio9.py
    Deleted an unused import

--- Ejercicio 9/Ejercicio9.py
+++ Ejercicio 9/Ejercicio9.py
@@ -11,9 +11,6 @@ Output: [3,4,1,2]
 Explanation: In the figure above, there is a sorted circular list of three elements. You are given a reference to the node with value 3, and we need to insert 2 into the list. The new node should be inserted between node 1 and node 3. After the insertion, the list should look like this, and we should still return node 3.
 '''
 
-from typing import List
-
-
 class Node:
     """
     Implementation of a node
