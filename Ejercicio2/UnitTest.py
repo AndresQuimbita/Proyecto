df7ea35943d9c0b2d641724f3881516183728a07
Juan505 <77290214+Juan505-9@users.noreply.github.com>
2021-12-12 22:15:22 -0500

    Update UnitTest.py

--- Ejercicio2/UnitTest.py
+++ Ejercicio2/UnitTest.py
@@ -5,12 +5,13 @@ import unittest
 class TestRandomized(unittest.TestCase):
 
     def testRandomizedSet_1(self):
-        self.assertEqual(input(["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"],[[], [1], [2], [2], [], [1], [2], []])),"Should be [null, true, false, true, Rand(1-2), true, false, 2]"
+        self.assertEqual(input([["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"],[[], [1], [2], [2], [], [1], [2], []]]),[None, True, False, True, 2, True, False, 2])
 
 
     def testRandomizedSet_2(self):
-        self.assertTrue(input(["RandomizedSet","remove", "insert","getRandom"],[[], [1], [1],[]])),"Should be [null, false, true, 1]"
+        self.assertTrue(input([["RandomizedSet","remove", "insert","getRandom"],[[], [1], [1],[]]]),[None, False, True, 1])
 
 
 if __name__ == '__main__':
     unittest.main()
+
