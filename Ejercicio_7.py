b7b56b09c79ac74d1ed113fa352e608150fac933
Juan505 <77290214+Juan505-9@users.noreply.github.com>
2021-12-13 19:56:33 -0500

    Add files via upload

---
+++ Ejercicio_7.py
@@ -0,0 +1,29 @@
+def count_paths(A):
+    n = len(A)
+    dic = {}
+    dic_prev = {}
+
+    for ind, val in enumerate(A):
+        for j in range(ind):
+            prev_val = A[j]
+            dif = val - prev_val
+            cur = (ind,dif)
+            prev = (j,dif)
+
+            if cur not in dic:
+                dic[cur] = 0
+
+            if cur not in dic_prev:
+                dic_prev[cur] = 1
+
+            else:
+                dic_prev[cur] +=1
+
+            if prev in dic_prev:
+                dic[cur] += dic[prev]
+            if prev in dic:
+                dic[cur] += dic_prev[prev]
+    return sum(dic.values())
+
+if __name__ == '__main__':
+    print(count_paths([2,7,9,4,5,6]))
