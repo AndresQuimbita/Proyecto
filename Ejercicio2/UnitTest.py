c07a1f9907d46e12925a4f33f3dafdc70df3f0c0
Juan505 <77290214+Juan505-9@users.noreply.github.com>
2021-12-13 22:57:52 -0500

    Update UnitTest.py

--- Ejercicio2/UnitTest.py
+++ Ejercicio2/UnitTest.py
@@ -5,19 +5,19 @@ import unittest
 class TestRandomized(unittest.TestCase):
 
     def testRandomizedSet_1(self):
-        self.assertEqual(input([["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"],[[], [1], [2], [2], [1], [1], [2], [2]]]),[None, True, False, True, 1, True, False, 2])
+        self.assertEqual(input(["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"],[[], [1], [2], [2], [], [1], [2], []]),[None, True, False, True, 1, True, False, 2])
 
     def testRandomizedSet_2(self):
-        self.assertTrue(input([["RandomizedSet","remove", "insert","getRandom"],[[], [1], [1],[1]]]),[None, False, True, 1])
+        self.assertTrue(input(["RandomizedSet","remove", "insert","getRandom"],[[], [1], [1],[1]]),[None, False, True, 1])
 
     def testRandomizedSet_3(self):
-        self.assertTrue(input([["RandomizedSet","insert", "remove","remove","insert","getRandom"],[[], [1], [1],[1],[5],[5]]]),[None, True, True,False,True ,5])
+        self.assertTrue(input([["RandomizedSet","insert", "remove","remove","insert","getRandom"],[[], [1], [1],[1],[5],[]]]),[None, True, True,False,True ,5])
 
     def testRandomizedSet_4(self):
-        self.assertTrue(input([["RandomizedSet","insert","insert","insert","insert","getRandom"],[[], [1], [2],[3],[1],[1]]]),[None, True, True,True,False,1])
+        self.assertTrue(input([["RandomizedSet","insert","insert","insert","insert","getRandom"],[[], [1], [2],[3],[1],[]]]),[None, True, True,True,False,1])
 
     def testRandomizedSet_5(self):
-        self.assertTrue(input([["RandomizedSet","insert","remove","remove","insert","getRandom"],[[], [4], [3],[4],[3],[3]]]),[None, True, False,True,True,3])
+        self.assertTrue(input([["RandomizedSet","insert","remove","remove","insert","getRandom"],[[], [4], [3],[4],[3],[]]]),[None, True, False,True,True,3])
 
 if __name__ == '__main__':
     unittest.main()
